2473cd8358cddaf9a8ba5aeaeb6f115f92f10923
laoqiu <laoqiu.com@gmail.com>
2012-03-22 15:15:07 +0800

    css and template update

--- pypress/local_settings.py
+++ pypress/local_settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-SQLALCHEMY_DATABASE_URI = "mysql://root:332527@localhost/pypress?charset=utf8"
+SQLALCHEMY_DATABASE_URI = "mysql://root@localhost/pypress?charset=utf8"
 SQLALCHEMY_DATABASE_ECHO = False
 
